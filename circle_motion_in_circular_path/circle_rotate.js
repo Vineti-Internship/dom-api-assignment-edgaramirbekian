e79c35d107905a39e810923a9ff06d2711f0c9b9
Edgar <amirbekianedgar@gmail.com>
2018-11-11 16:29:42 +0400

    added end button by DOM API, and it's functional to stop movement

--- circle_motion_in_circular_path/circle_rotate.js
+++ circle_motion_in_circular_path/circle_rotate.js
@@ -30,15 +30,26 @@ window.onload = function () {
     button_container.className = 'button_container';
     my_page.appendChild(button_container);
 
+    //create second button container
+    let button_container_2 = document.createElement('div');
+    button_container_2.className = "button_container_2";
+    my_page.appendChild(button_container_2);
+
     // create a button, place ir in button container
     let button = document.createElement('button');
     button.innerHTML = "START";
     button.className = "start_button";
-
     button_container.appendChild(button);
+
+    // create an end button
+    let end_button = document.createElement('button');
+    end_button.innerHTML = "END";
+    end_button.className = "end_button";
+    button_container_2.appendChild(end_button);
 
      // run this function when clicking START button
     button.onclick = function () {
+        
         // get element which will be used
         const ELEM = document.getElementById("ball");
 
@@ -53,26 +64,34 @@ window.onload = function () {
         var start_angle = 0;
 
         //end function when reaching this angle
-        var end_angle = 11760;
+        var end_angle = Infinity;
 
         //run function everi 5 ms
         var my_interval = setInterval (move_ball, 5);
         function move_ball () { 
+
             // when reach end_angle end interval 
             if (start_angle == end_angle) {
                 clearInterval (my_insterval);
             }
             else {
+                
                 // add 1 radian to primary angle every time function is called
                 start_angle += 1;
 
                 // add new coordinates according these mathematical calculations
+                // multiply start_angle with PI/180 becouse JS Math trigonomeric objects work with Radians rather than degrees
                 var x = x0 + RADIUS * Math.cos(start_angle * Math.PI/180);
                 var y = y0 - RADIUS * Math.sin(start_angle * Math.PI/180);
 
                 // assign new coordinates to element with id 'ball'
                 ELEM.style.left = x +'px';
                 ELEM.style.top = y + 'px';
+
+                // add end_button functional to stop movement
+                end_button.onclick = function () {
+                    end_angle = start_angle;
+                };
             }
         }    
     }
